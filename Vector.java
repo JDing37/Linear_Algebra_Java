b46ce566e63b7951be7ae0bb307ded6960e2e7ac
Joseph Dingillo <josephdingillo@Josephs-MacBook-Pro.local>
2022-12-29 19:18:48 -0600

    Update

--- Vector.java
+++ Vector.java
@@ -8,6 +8,7 @@ public class Vector extends Matrix{
 	public double[][] vector;
 	public double inverseMagnitude;
 	public double vectorProjection;
+	public double vectorAngle;
 
 	public double magnitude(double[][] vector) {
 		magnitude  = 0;
@@ -54,12 +55,33 @@ public class Vector extends Matrix{
 	}
 	
 	public double[][] unitVector(double[][] vector){
+		vector = new double[vector.length][1];
 		magnitude = magnitude(vector);
 		inverseMagnitude = 1/magnitude;
 		for(int i = 0; i < vector.length; i++) {
 			this.vector[i][0] = inverseMagnitude * vector[i][0];
 		}
 		return this.vector;
+	}
+	
+	public double angleBetweenVectors(double[][] v1, double[][] v2) {
+		vectorAngle = Math.acos((dotProduct(v1,v2))/(magnitude(v1)*magnitude(v2)));
+		return vectorAngle;
+	}
+	
+	public double[][] crossProduct(double[][] v1, double[][] v2){
+		vector = new double[v1.length][1];
+		if(v1[0].length == 3 && v2[0].length == 3) {
+			double[][] cutMatrix1 = {{v1[0][1],v1[0][2]},{v2[0][1],v2[0][2]}};
+			double[][] cutMatrix2 = {{v1[0][0],v1[0][2]},{v2[0][0],v2[0][2]}};
+			double[][] cutMatrix3 = {{v1[0][0],v1[0][1]},{v2[0][0],v2[0][1]}};
+			vector[0][0] = determinant(cutMatrix1);
+			vector[1][0] = -1*determinant(cutMatrix2);
+			vector[2][0] = determinant(cutMatrix3);
+			return vector;
+		} else {
+			throw new IllegalArgumentException("Please enter vectors as rows OR wrong dimension of vectors");
+		}
 	}
 	
 }
